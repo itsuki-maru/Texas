e1b12488ef98c06f30efd965f54a492a21e9d032
itsuki-maru <marumaru.developer@gmail.com>
2024-03-17 21:14:35 +0900

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 use clap::{App, Arg, SubCommand};
 use regex::Regex;
+use std::fs;
 use std::fs::File;
 use std::io::{self, BufRead, BufReader, Write, Read};
 use std::path::Path;
@@ -43,24 +44,41 @@ struct StatusData {
 }
 
 fn split_file(target_file: &str, regex_pattrern: &str) -> StatusData {
-    // let text = fs::read_to_string(target_file).unwrap();
-    let mut file = match File::open(target_file) {
-        Ok(file) => file,
-        Err(_) => return StatusData {
-            status_code: 400,
-            message: "File not found.".to_string(),
-        },
-    };
+    let text = fs::read_to_string(target_file).unwrap();
+    let lines = text.split("\n").collect::<Vec<_>>();
 
-    let mut contents = String::new();
-    match file.read_to_string(&mut contents) {
-        Ok(_) => StatusData {
-            status_code: 200,
-            message: "File read successfully.".to_string(),
-        },
-        Err(e) => StatusData {
-            status_code: 500,
-            message: format!("Error reading file: {}", e),
-        },
+    let mut index_list: Vec<u32> = Vec::new();
+    let re = Regex::new(&regex_pattrern).unwrap();
+    for (i, line) in lines.iter().enumerate() {
+        if re.is_match(line) {
+            index_list.push(i.try_into().unwrap());
+        }
+    }
+    index_list.push(lines.len().try_into().unwrap());
+
+    let mut data_range: Vec<u32> = Vec::new();
+    let mut i = 1;
+    for index in index_list {
+        let output = format!("./output_{}.txt", i);
+        data_range.push(index);
+        if data_range.len() == 2 {
+            let first: u32 = *data_range.first().unwrap();
+            let last: u32 = *data_range.last().unwrap();
+            let result = &lines[first as usize..last as usize];
+            let write_data = result.join("\n");
+
+            let mut f = File::create(output).unwrap();
+            let bytes = write_data.as_bytes();
+            f.write_all(bytes).unwrap();
+
+            let tmp_index: u32 = *data_range.last().unwrap();
+            data_range.clear();
+            data_range.push(tmp_index);
+            i += 1;
+        }
+    }
+    StatusData {
+        status_code: 200,
+        message: "File read successfully.".to_string(),
     }
 }
