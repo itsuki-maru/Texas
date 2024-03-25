f550a13ec6d7a7b980fbc738dcfd8aaa95e21180
itsuki-maru <marumaru.developer@gmail.com>
2024-03-25 22:18:35 +0900

    Update main.rs rustex.rs

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,13 @@ use clap::{App, Arg, SubCommand};
 mod scheme;
 mod rustex;
 use rustex::split_file;
+use std::env;
 
 fn main() {
+    // カレントディレクトリを取得
+    let current_directory = env::current_dir().expect("Current Directoru Get Error.");
+    let current_dir_str = current_directory.to_str().unwrap_or("Invalied Path.");
+
     let matches = App::new("rustex")
         .version("1.0")
         .author("Itsuki Maru")
@@ -20,18 +25,26 @@ fn main() {
                     .required(true)
                     .takes_value(true))
                 .arg(Arg::with_name("regex")
-                .short('r')
-                .long("regex")
-                .value_name("REGEX")
-                .help("Reguler expression for splitting.")
-                .required(true)
-                .takes_value(true))
+                    .short('r')
+                    .long("regex")
+                    .value_name("REGEX")
+                    .help("Reguler expression for splitting.")
+                    .required(true)
+                    .takes_value(true))
+                .arg(Arg::with_name("output")
+                    .short('o')
+                    .long("output")
+                    .value_name("OUTPUT")
+                    .help("Output directory.")
+                    .required(false)
+                    .takes_value(true)
+                    .default_value(current_dir_str))
         ).get_matches();
     
     // "split" ex) rustex split -t ./testfile/test1.txt -r "^第[1-9]章"
     if let Some(matches) = matches.subcommand_matches("split") {
-        if let (Some(target_file), Some(regex_pattrern)) = (matches.value_of("target"), matches.value_of("regex")) {
-            let result = split_file(target_file, regex_pattrern);
+        if let (Some(target_file), Some(regex_pattrern), Some(output_dir)) = (matches.value_of("target"), matches.value_of("regex"), matches.value_of("output")) {
+            let result = split_file(target_file, regex_pattrern, output_dir);
             println!("Status Code: {}, Message: {}", result.status_code, result.message);
             return;
         }
--- src/rustex.rs
+++ src/rustex.rs
@@ -1,58 +1,136 @@
 use regex::Regex;
-use std::fs;
+use std::{env, fs};
 use std::fs::File;
 use std::io::Write;
+use std::path::{PathBuf, Path};
 
 use crate::scheme::StatusData;
 
-fn get_abs_path(filename: String) -> String {
-    println!("{}", filename);
-    filename
+// ファイルの絶対パスを取得する関数
+fn get_abs_filepath(filename: &str) -> std::io::Result<PathBuf> {
+    let current_dir = env::current_dir()?;
+    Ok(current_dir.join(filename))
 }
 
-fn get_path_exists(abs_filepath: String) -> Option<bool> {
-    println!("{}", abs_filepath);
-    Some(true)
+// ディレクトリの絶対パスを取得する関数
+fn get_abs_directory_path(dir_name: &str) -> std::io::Result<PathBuf> {
+    let current_dir = env::current_dir()?;
+    Ok(current_dir.join(dir_name))
 }
 
+// パスがファイルか判定する関数
+fn is_file<P: AsRef<Path>>(path: P) -> bool {
+    fs::metadata(path).map(|metadata| metadata.is_file()).unwrap_or(false)
+}
+
+// パスがディレクトリか判定する関数
+fn is_dir<P: AsRef<Path>>(path: P) -> bool {
+    fs::metadata(path).map(|metadata| metadata.is_dir()).unwrap_or(false)
+}
+
+// "split"
 pub fn split_file(
     target_file: &str,
-    regex_pattrern: &str
+    regex_pattrern: &str,
+    output_directory: &str,
 ) -> StatusData {
 
-    let abs_target_file_path = get_abs_path(target_file.to_string());
-    println!("{}", abs_target_file_path);
+    // 対象ファイルの絶対パスを取得
+    let target_file_abs = match get_abs_filepath(target_file) {
+        Ok(path) => path,
+        Err(_) => return StatusData {
+            status_code: 404,
+            message: format!("{} is not found.", target_file),
+        },
+    };
 
-    let is_exists = get_path_exists(abs_target_file_path);
+    // ファイルの存在確認
+    if !target_file_abs.exists() {
+        return StatusData {
+            status_code: 404,
+            message: format!("{} is not found.", target_file),    
+        };
+    }
 
-    let text = fs::read_to_string(target_file).unwrap();
+    // 出力先の絶対パスを取得
+    let output_directory_abs = match get_abs_directory_path(output_directory) {
+        Ok(path) => path,
+        Err(_) => return StatusData {
+            status_code: 404,
+            message: format!("{} is not found.", output_directory),
+        },
+    };
+
+    // 出力先がディレクトリか確認
+    if !is_dir(&output_directory_abs) {
+        return StatusData {
+            status_code: 400,
+            message: format!("{} is not directory.", output_directory),    
+        };
+    }
+
+    // テキストファイルを読み込み
+    let text = match fs::read_to_string(&target_file_abs) {
+        Ok(content) => content,
+        Err(_) => return StatusData {
+            status_code: 500,
+            message: "Failed to read the file.".to_string(),
+        },
+    };
+
+    let re = match Regex::new(&regex_pattrern) {
+        Ok(r) => r,
+        Err(_) => return StatusData {
+            status_code: 500,
+            message: "Invalid regex pattern.".to_string(),
+        },
+    };
+
+    // 行を格納するベクタを定義
     let lines = text.split("\n").collect::<Vec<_>>();
 
-    let mut index_list: Vec<u32> = Vec::new();
-    let re = Regex::new(&regex_pattrern).unwrap();
-    for (i, line) in lines.iter().enumerate() {
+    // lines.iter().enumerate(): linesベクタの各要素に対してイテレーション（繰り返し処理）を行い、enumerateメソッドを用いてそれぞれの要素にインデックス番号を付与
+    // enumerateは各要素をタプル(i, line)の形で返します。ここでiは要素のインデックス（0から始まる）、lineはその要素の値（ここではファイルの各行のテキスト）
+    // filter_mapメソッドは、イテレータの各要素に対して関数を適用し、その関数がSome(value)を返した場合のみ、valueを新しいイテレータの要素として収集
+    // 正規表現reにマッチする行に対してのみ、その行のインデックス（i）をu32型にキャストしてSome(i as u32)として返す。マッチしない行はNoneを返し、その結果これらの行は無視
+    let mut index_list: Vec<u32> = lines.iter().enumerate().filter_map(|(i, line)| {
         if re.is_match(line) {
-            index_list.push(i.try_into().unwrap());
+            Some(i as u32)
+        } else {
+            None
         }
-    }
-    index_list.push(lines.len().try_into().unwrap());
+    }).collect(); // 最後に、filter_mapから返されるイテレータをcollectメソッドを使ってベクタに収集。この結果、index_listはマッチした行のインデックスのみを含むVec<u32>型のベクタとなる
+    // index_listベクタにファイルの総行数（lines.len()をu32にキャストした値）を追加しています。これにより、後続の処理でファイルの最後のセクションを正しく処理するための終端インデックスとして機能
+    index_list.push(lines.len() as u32);
+
 
     let mut data_range: Vec<u32> = Vec::new();
     let mut i = 1;
     for index in index_list {
-        let output = format!("./output_{}.txt", i);
+        let output_filename = format!("./output_{}.txt", i);
         data_range.push(index);
         if data_range.len() == 2 {
-            let first: u32 = *data_range.first().unwrap();
-            let last: u32 = *data_range.last().unwrap();
+            let first: u32 = *data_range.first().expect("First index missing");
+            let last: u32 = *data_range.last().expect("Last index missing");
             let result = &lines[first as usize..last as usize];
             let write_data = result.join("\n");
 
-            let mut f = File::create(output).unwrap();
-            let bytes = write_data.as_bytes();
-            f.write_all(bytes).unwrap();
+            let output_path = output_directory_abs.join(&output_filename);
+            let mut f = match File::create(output_path) {
+                Ok(file) => file,
+                Err(_) => return StatusData {
+                    status_code: 500,
+                    message: format!("Failed to create file: {}", output_filename),
+                }
+            };
+            if let Err(_) = f.write_all(write_data.as_bytes()) {
+                return StatusData {
+                    status_code: 500,
+                    message: format!("Failed to write to file: {}", output_filename),
+                };
+            }
 
-            let tmp_index: u32 = *data_range.last().unwrap();
+            let tmp_index: u32 = *data_range.last().expect("Last index missing after write");
             data_range.clear();
             data_range.push(tmp_index);
             i += 1;
